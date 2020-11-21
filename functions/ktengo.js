b43479997e26ab5937ce43d25cc134dae2ec30ed
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-11-21 16:37:41 +0100

    avanza #78 ya que añadida funcionalidad de enviar el mensaje a telegram [skip-ci]

--- functions/ktengo.js
+++ functions/ktengo.js
@@ -1,3 +1,21 @@
+const request = require('request-promise');
+const lib = require('./tareas')
+
+async function enviaMensaje(chat_id, text) {
+  const options = {
+    method: 'GET',
+    uri: `https://api.telegram.org/bot${process.env.TELEGRAMBOTTOKEN}/sendMessage`,
+    qs: {
+      chat_id,  //id del chat
+      text      //contenido
+    }
+  };
+
+  return request(options);
+}
+
+
+
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
