2c86b351ac1c9ffa04ddcced72f1b840c4a81a36
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-11-21 16:40:47 +0100

    avanza #1 ya que añadido switch opciones bot

--- functions/ktengo.js
+++ functions/ktengo.js
@@ -17,6 +17,24 @@ async function enviaMensaje(chat_id, text) {
 
 
 exports.handler = async function(event, context) {
+    let body = JSON.parse(event.body);
+    let {chat, text} = body.message;
+  
+    if (text){ //Contiene texto el mensaje, será el comando
+        let a_devolver = '';
+        switch (text) {
+            case "/ktengo":
+              a_devolver = lib.getAsignaturas();
+              break;
+            default:
+              a_devolver = "Usa /ktengo para saber que tareas tienes que realizar y para consultas más concretas: \n/ktengoIV para obtener las tareas a realizar de IV. \n/ktengoDAI para obtener las tareas a realizar de DAI. \n/ktengoSPSI para obtener las tareas a realizar de SPSI."
+              break;
+        }       
+        await enviaMensaje(chat.id, a_devolver);
+    }
+    else{ //no contiene texto el mensaje
+        await enviaMensaje(chat.id, 'Text message is expected.');
+    }
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Hello World"})
